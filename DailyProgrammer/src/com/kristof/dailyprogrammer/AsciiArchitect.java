93d190625d043a7b4de7c354d12536bb1cb8e74e
Josh Kristof <josh.kristof@gmail.com>
2014-04-17 12:02:49 -0700

    Solution added for #158 [Intermediate] Part 1 - The ASCII Architect

---
+++ DailyProgrammer/src/com/kristof/dailyprogrammer/AsciiArchitect.java
@@ -0,0 +1,43 @@
+package com.kristof.dailyprogrammer;
+
+import java.util.ArrayList;
+
+
+public class AsciiArchitect {
+
+    public static void main( String[] args ) {
+        String[] output = new String[10];
+        String template = "++--***...";
+        ArrayList<String> columns = new ArrayList<String>();
+        String builderString = args[0];
+
+        for ( int i = 0; i < builderString.length(); i++ ) {
+            String column = "";
+            if ( Character.isDigit( builderString.charAt( i ) ) ) {
+                for ( int j = 0; j < Character.getNumericValue( builderString.charAt( i ) ); j++ ) {
+                    column += " ";
+                }
+                column += template.substring( 0, builderString.charAt( i + 1 ) - 96 );
+            }
+            else if ( Character.isLetter( builderString.charAt( i ) ) && ( i == 0 || !Character.isDigit( builderString.charAt( i - 1 ) ) ) ) {
+                column += template.substring( 0, builderString.charAt( i ) - 96 );
+            }
+            if ( !column.equals( "" ) ) {
+                while ( column.length() < 10 )
+                    column += " ";
+                columns.add( 0, column );
+            }
+        }
+        for ( int i = 0; i < columns.size(); i++ ) {
+            for ( int j = 0, j2 = 9; j < 10; j++, j2-- ) {
+                if ( output[j] == null ) output[j] = "";
+                output[j] = columns.get( i ).charAt( j2 ) + output[j];
+            }
+        }
+
+        for ( String s : output ) {
+            System.out.println( s );
+        }
+    }
+
+}
